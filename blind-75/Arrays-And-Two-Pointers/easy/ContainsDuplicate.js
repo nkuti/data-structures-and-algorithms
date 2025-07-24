98c24a2030fe3781370fe2d3c616e740e219d5c8
Noelle Kuti <nkuti.dev@gmail.com>
2025-07-24 00:18:30 -0400

    contains duplicate (easy)

---
+++ blind-75/Arrays-And-Two-Pointers/easy/ContainsDuplicate.js
@@ -0,0 +1,63 @@
+/**
+ * || PROBLEM #   : CONTAINS DUPLICATE  ||
+ * fastest time: 16 min
+ * difficulty: easy
+ *
+ * Applicable pattern(s): Two Pointer
+ *
+ * || NOTES ||
+ *
+ *
+ *
+ *
+ */
+
+/** Problem description:
+ *
+ * Given an integer array nums, return true
+ * if any value appears more than once in the array,
+ * otherwise return false.
+ *
+ */
+
+// Show your work!
+
+/**
+ * ||*********************************************||
+ *                 ATTEMPT 1
+ * ||*********************************************||
+ * */
+
+const fn = (arrayOfIntegers) => {
+  let pointerA = 0;
+  let pointerB = arrayOfIntegers.length - 1;
+
+  let duplicatesExist = false;
+
+  while (pointerA < pointerB) {
+    if (
+      arrayOfIntegers[pointerA] === arrayOfIntegers[pointerA + 1] ||
+      arrayOfIntegers[pointerB] === arrayOfIntegers[pointerB - 1]
+    ) {
+      duplicatesExist = true;
+      return duplicatesExist;
+    }
+
+    pointerA++;
+    pointerB--;
+  }
+
+  return duplicatesExist;
+};
+
+//Examples
+console.log(fn([1, 2, 3, 3]));
+console.log(fn([1, 2, 3, 4]));
+
+console.log(fn([1, 2, 3, 4, 4, 5, 6, 7]));
+
+/**
+ * ||*********************************************||
+ *                 ATTEMPT 2
+ * ||*********************************************||
+ * */
