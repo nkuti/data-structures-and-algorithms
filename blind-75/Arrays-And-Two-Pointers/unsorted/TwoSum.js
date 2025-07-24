bf57d6e8059826ba4ec6a4becf2d3bfe59b95868
Noelle Kuti <nkuti.dev@gmail.com>
2025-07-23 23:25:23 -0400

    initial

---
+++ blind-75/Arrays-And-Two-Pointers/unsorted/TwoSum.js
@@ -0,0 +1,37 @@
+/**
+ * || PROBLEM #   :  ||
+ * fastest time:
+ * difficulty:
+ *
+ * Applicable pattern(s):
+ *
+ * || NOTES ||
+ *
+ *
+ *
+ *
+ */
+
+/** Problem description:
+ *
+ *Given an array of integers nums and an integer target, 
+    return the indices i and j such that 
+    nums[i] + nums[j] == target and i != j.
+
+    You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+
+    Return the answer with the smaller index first. 
+ */
+
+// Show your work!
+
+const fn = () => {
+  const arrayOfIntegers = [3, 6, 7, 3];
+
+  let pointerA = arrayOfIntegers[0];
+  let pointerB = arrayOfIntegers[arrayOfIntegers.length - 1];
+
+  //while()
+};
+
+fn();
---
+++ general/templates/leetcode-problem-attempt.js
@@ -0,0 +1,24 @@
+/**
+ * || PROBLEM #   :  ||
+ * fastest time:
+ * difficulty:
+ *
+ * Applicable pattern(s):
+ *
+ * || NOTES ||
+ *
+ *
+ *
+ *
+ */
+
+/** Problem description:
+ *
+ *
+ *
+ *
+ */
+
+// Show your work!
+
+const fn = () => {};
