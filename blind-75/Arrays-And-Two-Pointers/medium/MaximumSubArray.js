e541aa9e2ee5a439e4824dc68623dae34a5aa03f
Noelle Kuti <nkuti.dev@gmail.com>
2025-07-26 18:13:45 -0400

    struggled on maximumSubArray, learned about kadane algorithm, more practice needed

--- blind-75/Arrays-And-Two-Pointers/medium/MaximumSubArray.js
+++ blind-75/Arrays-And-Two-Pointers/medium/MaximumSubArray.js
@@ -1,16 +1,21 @@
 /**
  * || PROBLEM #   : Maximum Sub Array ||
  * dates attempted -- time:
- *      1:
+ *      1: several hours, and chatGPT help
  *      2:
  * difficulty: medium
  *
- * Applicable pattern(s):
+ * Applicable pattern(s): kadane algorithm
  *
  * || NOTES ||
  *
+ * On attempt 1 the thought process was "if the next number the pointer
+ * hits doesn't contribute to the total, we don't slide the window" which FAILS
+ * Then I tried to do "check from i to beginning of array, i to end, and track the highest sum"
+ * This ALSO FAILS, because the array could be entirely in the middle.
  *
- * 21:31
+ * Asked chatGPT and looked up the relevant algorithm
+ *
  *
  */
 
@@ -31,7 +36,24 @@
  * ||*********************************************||
  * */
 
-const fn = () => {};
+const fn = (arrayOfNums) => {
+  let currentSum = arrayOfNums[0];
+  let largestSum = arrayOfNums[0];
+
+  for (let i = 1; i < arrayOfNums.length; i++) {
+    if (currentSum + arrayOfNums[i] > arrayOfNums[i]) {
+      currentSum = currentSum + arrayOfNums[i];
+    } else {
+      currentSum = arrayOfNums[i]; // start new subarray
+    }
+
+    if (currentSum > largestSum) {
+      largestSum = currentSum;
+    }
+  }
+
+  return largestSum;
+};
 
 /**
  * ||*********************************************||
@@ -44,4 +66,4 @@ const fn2 = () => {};
 // Examples
 
 console.log(fn([2, -3, 4, -2, 2, 1, -1, 4])); //should be 8
-console.log(fn([-1])); //should be -1
+//console.log(fn([-1])); //should be -1
