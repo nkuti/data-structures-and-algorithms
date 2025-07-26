41f7b389d13fc6069815fd33dd935293dbde5fc8
Noelle Kuti <nkuti.dev@gmail.com>
2025-07-25 21:35:23 -0400

    set up the one I will do tomorrow

---
+++ blind-75/Arrays-And-Two-Pointers/medium/MaximumSubArray.js
@@ -0,0 +1,47 @@
+/**
+ * || PROBLEM #   : Maximum Sub Array ||
+ * dates attempted -- time:
+ *      1:
+ *      2:
+ * difficulty: medium
+ *
+ * Applicable pattern(s):
+ *
+ * || NOTES ||
+ *
+ *
+ * 21:31
+ *
+ */
+
+/** Problem description:
+ *
+ *  Given an array of integers nums, find the subarray with 
+ *  the largest sum and return the sum.
+
+ *  A subarray is a contiguous non-empty sequence of elements within an array.
+ *
+ */
+
+// Show your work!
+
+/**
+ * ||*********************************************||
+ *                 ATTEMPT 1
+ * ||*********************************************||
+ * */
+
+const fn = () => {};
+
+/**
+ * ||*********************************************||
+ *                 ATTEMPT 2
+ * ||*********************************************||
+ * */
+
+const fn2 = () => {};
+
+// Examples
+
+console.log(fn([2, -3, 4, -2, 2, 1, -1, 4])); //should be 8
+console.log(fn([-1])); //should be -1
